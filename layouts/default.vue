b2ee209492a0df6103ada5f191215e9b79d91680
Lozumi Everkine <fantasynico1026@gmail.com>
2023-12-02 01:39:48 +0800

    update: Update top bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,77 +1,83 @@
 <template>
-	<v-app>
-		<v-app-bar color="#6C63FF" app absolute elevate-on-scroll>
-			<v-btn to="/" icon>
-				<v-avatar>
-					<img src="../static/app.svg" alt="UIS Food Services">
-				</v-avatar>
-			</v-btn>
+    <v-app>
 
-			<v-toolbar-title class="white--text text-center">Food Services</v-toolbar-title>
+        <v-app-bar color="#3A8FB7" app absolute elevate-on-scroll>
+            <v-btn to="/" icon class="mx-2">
 
-			<v-spacer />
+                    <img
+                        src="../static/MdiFood.svg"
+                        alt="NPU Food"
+                        style="width: 25px; height: 25px"
+                    >
 
-			<v-badge color="pink" :content="cartItemsCount" :value="cartItemsCount" overlap>
-				<v-btn to="/cart" icon>
-					<v-icon>shopping_cart</v-icon>
-				</v-btn>
-			</v-badge>
-			<v-btn v-if="!$vuetify.theme.isDark" @click="changeTheme()" icon>
-				<v-icon>brightness_2</v-icon>
-			</v-btn>
+            </v-btn>
 
-			<v-btn v-if="$vuetify.theme.isDark" @click="changeTheme()" icon>
-				<v-icon>wb_sunny</v-icon>
-			</v-btn>
-		</v-app-bar>
-		<v-main class="bg-fixed" style="background-image: url('/chef.svg')">
-			<v-container>
-				<nuxt />
-			</v-container>
-		</v-main>
-		<v-footer dark padless>
-			<v-card color="#6C63FF" flat class="w-full white--text text-center">
-			<v-card-text>
-					<v-btn v-for="platform in socialIcons" :key="platform.icon"
-						class="mx-4 white--text" icon
-						:href="platform.url" rel="noopener"
-					>
-						<v-icon size="24px">{{ platform.icon }}</v-icon>
-					</v-btn>
-				</v-card-text>
+            <v-toolbar-title class="white--text text-center"><strong>工大美食</strong></v-toolbar-title>
+            <v-spacer/>
 
-				<v-card-text class="white--text">
-					{{ new Date().getFullYear() }} — <strong>Food Services</strong>
-				</v-card-text>
-			</v-card>
-		</v-footer>
-	</v-app>
+            <v-badge color="pink" :content="cartItemsCount" :value="cartItemsCount" overlap>
+                <v-btn to="/cart" icon>
+                    <v-icon>shopping_cart</v-icon>
+                </v-btn>
+            </v-badge>
+            <v-btn v-if="!$vuetify.theme.isDark" @click="changeTheme()" icon class="mx-2">
+                <v-icon>brightness_2</v-icon>
+            </v-btn>
+
+            <v-btn v-if="$vuetify.theme.isDark" @click="changeTheme()" icon class="mx-2">
+                <v-icon>wb_sunny</v-icon>
+            </v-btn>
+
+        </v-app-bar>
+
+        <v-main class="bg-fixed" style="background-image: url('/chef.svg')">
+            <v-container>
+                <nuxt/>
+            </v-container>
+        </v-main>
+        <v-footer dark padless>
+            <v-card color="#3A8FB7" flat class="w-full white--text text-center">
+<!--                <v-card-text>-->
+<!--                    <v-btn v-for="platform in socialIcons" :key="platform.icon"-->
+<!--                           class="mx-4 white&#45;&#45;text" icon-->
+<!--                           :href="platform.url" rel="noopener"-->
+<!--                    >-->
+<!--                        <v-icon size="24px">{{ platform.icon }}</v-icon>-->
+<!--                    </v-btn>-->
+<!--                </v-card-text>-->
+
+                <v-card-text class="white--text">
+                    {{ new Date().getFullYear() }} — <strong>工大美食</strong>
+                </v-card-text>
+            </v-card>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
+import {mapMutations} from 'vuex';
 
 export default {
-	data () {
-		return {
-			socialIcons: [
-				{ icon: 'mdi-twitter', url: '#' },
-				{ icon: 'mdi-linkedin', url: '#' },
-				{ icon: 'mdi-instagram', url: '#' },
-				{ icon: 'mdi-email', url: '#' },
-			],
-		}
-	},
-	computed: {
-		cartItemsCount() {
-			return this.$store.state.cart.cartProducts.length;
-		}
-	},
-	methods: {
-		changeTheme() {
-			if (!this.$vuetify) return
-			this.$vuetify.theme.isDark = !this.$vuetify.theme.isDark;
-		}
-	}
+    data() {
+        return {
+            socialIcons: [
+                {icon: 'mdi-twitter', url: '#'},
+                {icon: 'mdi-linkedin', url: '#'},
+                {icon: 'mdi-instagram', url: '#'},
+                {icon: 'mdi-email', url: '#'},
+            ],
+        }
+    },
+    computed: {
+        cartItemsCount() {
+            return this.$store.state.cart.cartProducts.length;
+        }
+    },
+    methods: {
+        changeTheme() {
+            if (!this.$vuetify) return
+            this.$vuetify.theme.isDark = !this.$vuetify.theme.isDark;
+        }
+    }
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
   */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || 'Food Services',
+    title: process.env.npm_package_name || 'NPU Food',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
