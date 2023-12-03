1890c3f15211cc5d3d283346c3350f9734cbd1ef
Lozumi Everkine <fantasynico1026@gmail.com>
2023-12-03 22:13:26 +0800

    update: Update main bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,35 @@
 <template>
     <v-app>
         <v-app-bar color="#3A8FB7" app absolute elevate-on-scroll>
-            <v-btn to="/" icon class="mx-2">
-                <img src="../static/MdiFood.svg" alt="NPU Food" style="width: 24px; height: 24px">
-            </v-btn>
-
-            <v-toolbar-title class="white--text text-center"><strong>工大美食</strong></v-toolbar-title>
+            <img src="../static/npu-logo.png" alt="NPU"  class="ml-2" style="width: 24px; height: 24px">
+            <v-toolbar-title class="white--text text-center mx-2"><strong>工大美食</strong></v-toolbar-title>
+            <img class="mr-2" src="../static/MdiFood.svg" alt="NPU Food" style="width: 24px; height: 24px">
             <v-spacer/>
-
-            <v-btn class="mx-2" icon @click="openLoginDialog">
-                <v-icon>mdi-account-circle</v-icon>
+            <v-btn to="/" icon class="mx-2">
+                <v-icon>home</v-icon>
             </v-btn>
             <v-badge color="pink" :content="cartItemsCount" :value="cartItemsCount" overlap>
-                <v-btn to="/cart" icon>
+                <v-btn to="/cart" icon class="">
                     <v-icon>shopping_cart</v-icon>
                 </v-btn>
             </v-badge>
-            <v-btn v-if="!$vuetify.theme.isDark" @click="changeTheme()" icon class="mx-2">
+            <v-btn class="" icon @click="openLoginDialog">
+                <v-icon>mdi-account-circle</v-icon>
+            </v-btn>
+            <v-btn v-if="!$vuetify.theme.isDark" @click="changeTheme()" icon class="mr-2">
                 <v-icon>brightness_2</v-icon>
             </v-btn>
-            <v-btn v-if="$vuetify.theme.isDark" @click="changeTheme()" icon class="mx-2">
+            <v-btn v-if="$vuetify.theme.isDark" @click="changeTheme()" icon class="mr-2">
                 <v-icon>wb_sunny</v-icon>
             </v-btn>
         </v-app-bar>
 
-        <v-main class="bg-fixed" style="background-image: url('/chef.svg')">
+        <v-main class="bg-fixed" style="background-image: url('/npu-motto.png'); background-size: 40%; background-position: left top;">
             <v-container>
                 <nuxt/>
             </v-container>
         </v-main>
+
 
         <v-footer dark padless>
             <v-card color="#3A8FB7" flat class="w-full white--text text-center">
@@ -40,9 +41,15 @@
                         <v-icon size="24px">{{ platform.icon }}</v-icon>
                     </v-btn>
                 </v-card-text>
+<!--                <v-card-text class="white&#45;&#45;text">-->
+<!--                    {{ new Date().getFullYear() }} — <strong>工大美食</strong>-->
+<!--                </v-card-text>-->
                 <v-card-text class="white--text">
-                    {{ new Date().getFullYear() }} — <strong>工大美食</strong>
+                    西北工业大学 软件工程导论课程（2023-2024秋）大作业
+                    <br >
+                    Dobug小组作品
                 </v-card-text>
+
             </v-card>
         </v-footer>
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/chef.svg' },
+      { rel: 'icon', type: 'image/x-icon', href: '/npu-logo.png' },
       { rel: 'stylesheet', defer: true, href: 'https://fonts.googleapis.com/css?family=Material+Icons' },
     ]
   },
